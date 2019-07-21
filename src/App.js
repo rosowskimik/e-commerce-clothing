a075bffce932a8aa552fd714c5260f8261e31654
rosowskimik <m.rosowski1@wp.pl>
2019-07-21 12:51:23 +0200

    Added basic routing & menu-items paths

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, { Fragment } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component';
+import Hats from './pages/hats/hats.component';
+
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Fragment>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/shop/hats' component={Hats} />
+      </Switch>
+    </Fragment>
   );
 };
 
---
+++ src/pages/hats/hats.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './hats.styles.scss';
+
+const Hats = () => {
+  return <div>Placeholder</div>;
+};
+
+export default Hats;
