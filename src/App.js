f5f0e1c8eea17ee231cecabe580eb81ac1332557
rosowskimik <m.rosowski1@wp.pl>
2019-07-21 14:40:20 +0200

    Header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
@@ -9,6 +10,7 @@ import './App.css';
 const App = () => {
   return (
     <Fragment>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={ShopPage} />
