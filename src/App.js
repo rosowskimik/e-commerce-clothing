f1127698091b0d2598d8f8af1f851fa679cfe798
rosowskimik <m.rosowski1@wp.pl>
2019-07-21 14:13:16 +0200

    Responsive Shop-page, collection-preview component & collection-item component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component';
-import Hats from './pages/hats/hats.component';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
 
@@ -11,7 +11,7 @@ const App = () => {
     <Fragment>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route exact path='/shop/hats' component={Hats} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </Fragment>
   );
