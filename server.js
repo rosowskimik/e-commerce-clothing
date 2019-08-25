a138bde22b04c2de03ee54068931cbc681dffd98
rosowskimik <m.rosowski1@wp.pl>
2019-08-25 17:13:26 +0200

    Updated app to be PWA compliant
    Updated manifest
    Enforced HTTPS
    Added service-worker

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
 const compression = require('compression');
+const enforce = require('express-sslify');
+const cors = require('cors');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const app = express();
 
-app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(compression());
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 app.use('/payment', require('./routes/payment'));
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'service-worker.js'));
+});
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
